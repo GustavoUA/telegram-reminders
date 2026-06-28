73261dc27d8ff1137f1b1e5af26e216eb273c254
TH3HACKTF <168471099+GustavoUA@users.noreply.github.com>
2026-06-28 18:40:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
+
 import os
 import requests
-from datetime import datetime
+from datetime import datetime, UTC
+
+print("🚀 Iniciando Hermes...")
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
@@ -10,12 +13,23 @@ mensaje = f"""🤖 Hermes
 ✅ GitHub Actions funcionando correctamente.
 
 Fecha:
-{datetime.utcnow().strftime('%d/%m/%Y %H:%M UTC')}
+{datetime.now(UTC).strftime('%d/%m/%Y %H:%M UTC')}
 """
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-requests.post(url, data={
-    "chat_id": CHAT_ID,
-    "text": mensaje
-})
+respuesta = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": mensaje
+    },
+    timeout=30
+)
+
+print("Código HTTP:", respuesta.status_code)
+print("Respuesta Telegram:", respuesta.text)
+
+respuesta.raise_for_status()
+
+print("✅ Mensaje enviado correctamente.")
